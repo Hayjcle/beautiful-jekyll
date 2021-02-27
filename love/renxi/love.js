741b09d94e0b1b308f5bd235b4864a203c8127d7
cle <hayicle.vn@gmail.com>
2021-02-27 21:57:47 +0700

    update text

--- love/renxi/love.js
+++ love/renxi/love.js
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 微软雅黑,Verdana";
-            ctx.fillText("Đừng nhấn ^^!", 23, 10);
+            ctx.fillText("Nothing here ^^!", 23, 10);
             ctx.restore();
         },
         clear: function() {
